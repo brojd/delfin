e76890f3106da04cb98b68b45adf697baa3f77d3
dominik791 <dominik.broj@gmail.com>
2016-11-13 10:29:35 +0100

    create initial version of Nav component

--- front-admin/src/components/Main/Main.component.js
+++ front-admin/src/components/Main/Main.component.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 import Nav from '../../components/Nav/Nav.component';
+import classNames from 'classnames';
+import styles from './Main.stylsheet.css';
 
 class Main extends Component {
   render() {
     return (
-      <div>
+      <main className={classNames(styles.Main, 'uk-align-center uk-width-8-10')}>
         <Nav />
         {this.props.children}
-      </div>
+      </main>
     )
   }
 }
--- front-admin/src/components/Nav/Nav.component.js
+++ front-admin/src/components/Nav/Nav.component.js
@@ -1,15 +1,55 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+import classNames from 'classnames';
+import styles from'./Nav.stylesheet.css';
 
 class Nav extends Component {
   render() {
     return (
-      <ul className="uk-nav">
-        <li><Link to='/'>ZAWODY</Link></li>
-        <li><Link to='/swimmers'>ZAWODNICY</Link></li>
-        <li><Link to='/times'>WYNIKI</Link></li>
-        <li><Link to='/classifications'>KLASYFIKACJE</Link></li>
-      </ul>
+      <div className={classNames(styles.NavWrapper, 'uk-width-1-10')}>
+        <ul className={classNames("uk-nav", styles.Nav)}>
+          <li className='uk-margin-large-bottom'>
+            <Link to='/' className={styles.Nav__link}>
+              <div className='uk-text-center'>
+                <i className="uk-icon-home uk-icon-large"></i>
+              </div>
+              <div className='uk-text-center'>
+                ZAWODY
+              </div>
+            </Link>
+          </li>
+          <li className='uk-margin-large-bottom'>
+            <Link to='/swimmers' className={styles.Nav__link}>
+              <div className='uk-text-center'>
+                <i className="uk-icon-group uk-icon-large"></i>
+              </div>
+              <div className='uk-text-center'>
+                ZAWODNICY
+              </div>
+            </Link>
+          </li>
+          <li className='uk-margin-large-bottom'>
+            <Link to='/times' className={styles.Nav__link}>
+              <div className='uk-text-center'>
+                <i className="uk-icon-clock-o uk-icon-large"></i>
+              </div>
+              <div className='uk-text-center'>
+                WYNIKI
+              </div>
+            </Link>
+          </li>
+          <li>
+            <Link to='/classifications' className={styles.Nav__link}>
+              <div className='uk-text-center'>
+                <i className="uk-icon-trophy uk-icon-large"></i>
+              </div>
+              <div className='uk-text-center'>
+                KLASYFIKACJE
+              </div>
+            </Link>
+          </li>
+        </ul>
+      </div>
     )
   }
 }
