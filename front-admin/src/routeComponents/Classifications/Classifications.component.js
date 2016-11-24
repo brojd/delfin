9f4aa525606c19ac5a7db8a4c16a376c04a87f5f
Dominik Broj <dominik.broj@dataart.com>
2016-11-24 18:15:58 +0100

    enable choosing race in classifications

--- front-admin/src/routeComponents/Classifications/Classifications.component.js
+++ front-admin/src/routeComponents/Classifications/Classifications.component.js
@@ -1,13 +1,78 @@
 import React, {Component} from 'react';
+import ChooseRace from '../../components/ChooseRace/ChooseRace.component';
+import axios from 'axios';
+import CONFIG from '../../config';
 
-class Nav extends Component {
+class Classifications extends Component {
+  constructor() {
+    super();
+    this._getCategory = this._getCategory.bind(this);
+    this._updateRaceId = this._updateRaceId.bind(this);
+    this.state = {
+      raceId: '',
+      raceSwimmers: [],
+      competitionSwimmers: []
+    }
+  }
+  _updateRaceId(raceId) {
+    let raceSwimmers = this.state.competitionSwimmers.filter((n) => n.raceIds.includes(raceId));
+    this.setState({
+      raceId: raceId,
+      raceSwimmers: raceSwimmers
+    });
+  }
+  _getCategory(sex, style, age) {
+    if (age.value == 'W1' && sex.value == 'P1' && style.value == 'S1') {
+      this._updateRaceId(1);
+    } else if (age.value == 'W1' && sex.value == 'P1' && style.value == 'S2') {
+      this._updateRaceId(2);
+    } else if (age.value == 'W1' && sex.value == 'P1' && style.value == 'S3') {
+      this._updateRaceId(3);
+    } else if (age.value == 'W1' && sex.value == 'P2' && style.value == 'S1') {
+      this._updateRaceId(4);
+    } else if (age.value == 'W1' && sex.value == 'P2' && style.value == 'S2') {
+      this._updateRaceId(5);
+    } else if (age.value == 'W1' && sex.value == 'P2' && style.value == 'S3') {
+      this._updateRaceId(6);
+    } else if (age.value == 'W2' && sex.value == 'P1' && style.value == 'S1') {
+      this._updateRaceId(7);
+    } else if (age.value == 'W2' && sex.value == 'P1' && style.value == 'S2') {
+      this._updateRaceId(8);
+    } else if (age.value == 'W2' && sex.value == 'P1' && style.value == 'S3') {
+      this._updateRaceId(9);
+    } else if (age.value == 'W2' && sex.value == 'P2' && style.value == 'S1') {
+      this._updateRaceId(10);
+    } else if (age.value == 'W2' && sex.value == 'P2' && style.value == 'S2') {
+      this._updateRaceId(11);
+    } else if (age.value == 'W2' && sex.value == 'P2' && style.value == 'S3') {
+      this._updateRaceId(12);
+    }
+  }
+  componentDidMount() {
+    let currentCompetitionId = localStorage.getItem('currentCompetitionId');
+    this.setState({ currentCompetitionId: currentCompetitionId });
+    axios.all([
+        axios.get(`${CONFIG.API_URL}/competitions/${currentCompetitionId}/swimmers`),
+        axios.get(`${CONFIG.API_URL}/competitions/${currentCompetitionId}/schools`)
+      ])
+      .then(axios.spread((swimmersRes, schoolsRes) => {
+        let raceSwimmers = swimmersRes.data.filter((n) => n.raceIds.includes(this.state.raceId));
+        this.setState({
+          competitionSwimmers: swimmersRes.data,
+          raceSwimmers: raceSwimmers,
+          schools: schoolsRes.data
+        });
+      }))
+      .catch((error) => console.error(error));
+  }
   render() {
     return (
       <div>
-        klasyfikacje
+        <ChooseRace getCategory={this._getCategory}/>
+        {this.state.raceId}
       </div>
     )
   }
 }
 
-export default Nav;
+export default Classifications;
