1f4d38454614e3d5d8ad305551f635b22ee54476
Dominik Broj <dominik.broj@dataart.com>
2016-11-24 15:34:39 +0100

    add schools to backend and navigation

---
+++ front-admin/src/routeComponents/Schools/Schools.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Schools extends Component {
+  render() {
+    return (
+      <div>
+        szkoły
+      </div>
+    )
+  }
+}
+
+export default Schools;
