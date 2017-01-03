88e8a439946f39fda5890dbb6258f7c7f9c2b846
Dominik Broj <dominik.broj@dataart.com>
2017-01-03 10:43:28 +0100

    make footer stick to the bottom

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,16 @@
 import React, {Component} from 'react';
+import styles from './App.stylesheet.css';
 import Header from './components/Header/Header.component';
 import Footer from './components/Footer/Footer.component';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        {this.props.children}
+      <div>
+        <div className={styles.pageWrap}>
+          <Header />
+          {this.props.children}
+        </div>
         <Footer />
       </div>
     );
